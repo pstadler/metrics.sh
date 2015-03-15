3ba708dcfb5634c2bf958515b44aa8998e9d7f51
Patrick Stadler <patrick.stadler@gmail.com>
2015-03-15 21:53:34 +0100

    fix loader for dash

--- lib/utils/loader.sh
+++ lib/utils/loader.sh
@@ -1,24 +1,26 @@
 #!/bin/sh
 
-load_metric_with_prefix () {
+load_reporter_with_prefix () {
   local prefix=$1
   local file=$2
-
-  local content=$(sed \
+  # dash will error if this variable is defined as `local`
+  content=$(sed \
           -e 's/^[[:space:]]*\(init[ ]*()[ ]*{\)/'"$prefix"'\1/' \
-          -e 's/^[[:space:]]*\(collect[ ]*()[ ]*{\)/'"$prefix"'\1/' \
+          -e 's/^[[:space:]]*\(report[ ]*()[ ]*{\)/'"$prefix"'\1/' \
           -e 's/^[[:space:]]*\(terminate[ ]*()[ ]*{\)/'"$prefix"'\1/' \
           -e 's/^[[:space:]]*\(docs[ ]*()[ ]*{\)/'"$prefix"'\1/' $file)
 
   eval "$content"
 }
 
-load_reporter_with_prefix () {
+load_metric_with_prefix () {
   local prefix=$1
   local file=$2
-  local content=$(sed \
+
+  # dash will error if this variable is defined as `local`
+  content=$(sed \
           -e 's/^[[:space:]]*\(init[ ]*()[ ]*{\)/'"$prefix"'\1/' \
-          -e 's/^[[:space:]]*\(report[ ]*()[ ]*{\)/'"$prefix"'\1/' \
+          -e 's/^[[:space:]]*\(collect[ ]*()[ ]*{\)/'"$prefix"'\1/' \
           -e 's/^[[:space:]]*\(terminate[ ]*()[ ]*{\)/'"$prefix"'\1/' \
           -e 's/^[[:space:]]*\(docs[ ]*()[ ]*{\)/'"$prefix"'\1/' $file)
 
